c7132f200394bcf57fbaf9a31534c51f0aa41aa2
Kaplynenko <93135352+Kaplynenko@users.noreply.github.com>
2022-07-09 15:44:54 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,26 @@
-export const App = () => {
-  return <div>Hello</div>;
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+modele.id = nanoid();
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return (
+      <form>
+        <label>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+      </form>
+    );
+  }
+}
+export default App;
